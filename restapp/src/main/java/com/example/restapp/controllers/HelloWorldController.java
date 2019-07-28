ad4bf392b99ad2bb2542fc797f27b28d06d5aebb
Anton Danylov <ur.anton.danilov@gmail.com>
2019-07-28 21:07:25 +0300

    Added HelloWorld rest controller

---
+++ restapp/src/main/java/com/example/restapp/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.restapp.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+
+}
